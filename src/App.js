7fe913d068bf6b4a9489be12443de8578233ef79
Eric Santos <e.santos@joincea.com>
2021-05-26 12:15:02 -0600

    basic pwa, added navbar with toggle functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
+
+const Home = lazy(() => import('./components/Home'))
+// const About = lazy(() => import('./components/About'))
+// const JoinCea = lazy(() => import('./components/JoinCea'))
+// const Login = lazy(() => import('./components/Login'))
+// const Work = lazy(() => import('./components/Work'))
+// const Hiring = lazy(() => import('./components/Hiring'))
+// const Contact = lazy(() => import('./components/Contact'))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <AnimatePresence>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              {/* <Route path="/About" component={About} />
+              <Route path="/join" component={JoinCea} />
+              <Route path="/login" component={Login} />
+              <Route path="/work" component={Work} />
+              <Route path="/hiring" component={Hiring} />
+              <Route path="/contact" component={Contact} /> */}
+              {/* <Route path="/Robots" component={Robots} /> */}
+            </Switch>
+          </AnimatePresence>
+        </Suspense>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { FiMenu } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+import '../styles/navbar.css'
+
+const Navbar = () => {
+  const [active, setActive] = useState(false)
+
+  const handleClick = () => {
+    setActive(!active)
+    console.log(active)
+  }
+
+  return (
+    <>
+      <div className="icon-bar">
+        <FiMenu size="30" className="icon" onClick={handleClick} />
+      </div>
+      <nav className={active ? 'links' : 'open'}>
+        <Link to="/" className="link">
+          Home
+        </Link>
+        <Link to="/" className="link">
+          Home
+        </Link>
+        <Link to="/" className="link">
+          Home
+        </Link>
+        <Link to="/" className="link">
+          Home
+        </Link>
+        <Link to="/" className="link">
+          Home
+        </Link>
+        <Link to="/" className="link">
+          Home
+        </Link>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
